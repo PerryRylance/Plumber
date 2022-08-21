fbf49b3a2827a0ee593e630607fd4f8061273cb6
Perry Rylance <perryrylance@googlemail.com>
2022-08-21 07:13:31 +0100

    Stricter type declarations

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,1 +1,6 @@
-declare module '*';
+declare module 'pathfinding';
+declare module 'pathfinding/src/core/Util';
+declare module 'pathfinding/src/core/Grid';
+declare module 'pathfinding/src/core/Heuristic';
+declare module 'heap';
+declare module 'pseudo-random';
